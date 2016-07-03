59d860c743cd53826bd30ec753be5d913a9edb07
rh <ryanjharvey88@gmail.com>
2016-07-02 19:27:36 -0700

    Added views for SignUpActivity

--- app/src/main/java/com/epicodus/parkr/SignUpActivity.java
+++ app/src/main/java/com/epicodus/parkr/SignUpActivity.java
@@ -1,13 +1,25 @@
 package com.epicodus.parkr;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    @Bind(R.id.signupHeadline) TextView mHeadline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        ButterKnife.bind(this);
+
+        Typeface newFont = Typeface.createFromAsset(getAssets(), "fonts/seas.ttf");
+        mHeadline.setTypeface(newFont);
     }
 }
