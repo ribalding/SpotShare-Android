c84bfb4319d9d135a24bced1b4c2b386df4bce07
rh <ryanjharvey88@gmail.com>
2016-07-15 10:07:37 -0700

    Created spot model

--- app/src/main/java/com/epicodus/parkr/Spot.java
+++ app/src/main/java/com/epicodus/parkr/Spot.java
@@ -1,7 +1,60 @@
 package com.epicodus.parkr;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by Guest on 7/12/16.
  */
 public class Spot {
+    private String ownerID;
+    private String renterID;
+    private boolean isCurrentlyRented;
+    private String description;
+    private LatLng latLng;
+
+    public Spot(String ownerID, String description, LatLng latLng){
+        this.ownerID = ownerID;
+        this.description = description;
+        this.latLng = latLng;
+    }
+
+    public String getOwnerID(){
+        return this.ownerID;
+    }
+
+    public String getRenterID(){
+        return this.renterID;
+    }
+
+    public boolean getIsCurrentlyRented(){
+        return this.isCurrentlyRented;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public LatLng getLatLng(){
+        return getLatLng();
+    }
+
+    public void setOwnerID(String ownerID) {
+        this.ownerID = ownerID;
+    }
+
+    public void setRenterID(String renterID) {
+        this.renterID = renterID;
+    }
+
+    public void setCurrentlyRented(boolean currentlyRented) {
+        isCurrentlyRented = currentlyRented;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setLatLng(LatLng latLng) {
+        this.latLng = latLng;
+    }
 }
