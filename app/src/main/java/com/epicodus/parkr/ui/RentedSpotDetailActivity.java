551d2ff2abbfb50d9de0ed77dbef226317361672
rh <ryanjharvey88@gmail.com>
2016-07-24 21:48:08 -0700

    Rented Spot Detail Activity will now display full information about the selected spot

--- app/src/main/java/com/epicodus/parkr/ui/RentedSpotDetailActivity.java
+++ app/src/main/java/com/epicodus/parkr/ui/RentedSpotDetailActivity.java
@@ -2,6 +2,7 @@ package com.epicodus.parkr.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.epicodus.parkr.R;
 import com.epicodus.parkr.models.Spot;
@@ -10,10 +11,17 @@ import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class RentedSpotDetailActivity extends AppCompatActivity {
     private ArrayList<Spot> mSpots = new ArrayList<>();
+    @Bind(R.id.spotDetailAddress) TextView mSpotDetailAddress;
+    @Bind(R.id.spotDetailDescription) TextView mSpotDetailDescription;
+    @Bind(R.id.spotDetailStartDate) TextView mSpotDetailStartDate;
+    @Bind(R.id.spotDetailStartTime) TextView mSpotDetailStartTime;
+    @Bind(R.id.spotDetailEndDate) TextView mSpotDetailEndDate;
+    @Bind(R.id.spotDetailEndTime) TextView mSpotDetailEndTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,14 @@ public class RentedSpotDetailActivity extends AppCompatActivity {
 
         mSpots = Parcels.unwrap(getIntent().getParcelableExtra("spots"));
         int startingPosition = getIntent().getIntExtra("position", 0);
+
+        Spot currentSpot = mSpots.get(startingPosition);
+        mSpotDetailAddress.setText("Address: " + currentSpot.getAddress());
+        mSpotDetailDescription.setText("Description: " + currentSpot.getDescription());
+        mSpotDetailStartDate.setText("Start Date:" + currentSpot.getStartDate());
+        mSpotDetailStartTime.setText("Start Time: " + currentSpot.getStartTime());
+        mSpotDetailEndDate.setText("End Date: " + currentSpot.getEndDate());
+        mSpotDetailEndTime.setText("End Time " + currentSpot.getEndTime());
 
     }
 }
